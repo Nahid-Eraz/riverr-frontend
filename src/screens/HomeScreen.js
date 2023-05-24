fe45fa7a9c9bee3597154a76e7fac455c749a4ce
Nahid Eraz <nahidulislameraz@gmail.com>
2023-05-24 14:48:04 +0600

    add npmrc file for peer error, add react  bootstrap, react router bootstrap

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import React from 'react';
 //import data from '../data';
 import axios from 'axios';
 import { useEffect, useReducer, useState } from 'react';
